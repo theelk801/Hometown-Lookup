9c5b3dc1c554facd59465b6810a7c7ccdab8ba61
Evan Kranzler <theelk801@gmail.com>
2020-06-10 19:05:30 -0400

    Updates

---
+++ precache-manifest.448503a53660411a10856313d07b6a04.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d032f85c949c7580b9d507e251007747",
+    "url": "/Hometown-Lookup/index.html"
+  },
+  {
+    "revision": "cf2aa4f9f8f8d1bddce7",
+    "url": "/Hometown-Lookup/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "c5abeb672e42d04c01ae",
+    "url": "/Hometown-Lookup/static/css/main.07aeab6e.chunk.css"
+  },
+  {
+    "revision": "cf2aa4f9f8f8d1bddce7",
+    "url": "/Hometown-Lookup/static/js/2.7874ced6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Hometown-Lookup/static/js/2.7874ced6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5abeb672e42d04c01ae",
+    "url": "/Hometown-Lookup/static/js/main.b28df2fb.chunk.js"
+  },
+  {
+    "revision": "308acab2fc214a760a14",
+    "url": "/Hometown-Lookup/static/js/runtime-main.9bdabc56.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Hometown-Lookup/static/media/logo.5d5d9eef.svg"
+  }
+]);
