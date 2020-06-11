893b3cc46310770a8cfb559a244b7d5dcd6f0657
Evan Kranzler <theelk801@gmail.com>
2020-06-11 17:41:52 -0400

    updated copy and formatting

--- src/App.js
+++ src/App.js
@@ -70,44 +70,63 @@ class App extends Component {
   state = {};
   render() {
     return (
-      <div className="container">
-        <div className="component summary">
-          To test this prototype, select your home state, type your town, and
-          choose one of the example prompts. Click the link to search.
-        </div>
-        <div className="component summary">
-          Prompts are just examples for right now
-        </div>
-        <div className="selector">
-          <Dropdown
-            className="dropdown"
-            options={states}
-            onChange={this.chooseState}
-            value="Select a State"
-          />
-          <div className="component">
-            Selected state: {this.state.selectedState}
+      <div className="outer">
+        <div className="container">
+          <h2>Untitled Project: Hometown</h2>
+          <div className="component summary">
+            Goal 1: Learn about your hometown's history of discrimination as
+            well as current issues
           </div>
-        </div>
-        <div className="selector">
-          <div className="component">Enter your town</div>
-          <input onChange={this.chooseTown} type="text" />
-          <div className="component">
-            Selected town: {this.state.selectedTown}
+          <div className="component summary">
+            Goal 2: Unlearn the use of "growing up in a liberal or progressive
+            town" as a defense against learning about racism and discrimination
+            that impacted your upbringing
           </div>
-        </div>
-        <div className="selector">
-          <Dropdown
-            className="dropdown"
-            options={prompts}
-            onChange={this.choosePrompt}
-            value="Select a Prompt"
-          />
-          <div className="component">
-            Selected prompt: {this.state.selectedPrompt}
+          <div className="component summary">
+            Goal 3: Learn relevant examples in your hometown to use when
+            speaking with parents and family who struggle to connect with your
+            experiences or examples
           </div>
+          <div className="smaller">
+            <div className="component summary">
+              To test this prototype, select your home state, type in your town,
+              and choose one of the example prompts. Click the link to search.
+            </div>
+            <div className="component summary">
+              Please DM me for prompt edits and suggestions.
+            </div>
+          </div>
+          <div className="selector">
+            <Dropdown
+              className="dropdown"
+              options={states}
+              onChange={this.chooseState}
+              value="Select a State"
+            />
+            <div className="component">
+              Selected state: {this.state.selectedState}
+            </div>
+          </div>
+          <div className="selector">
+            <div className="component">Enter your town</div>
+            <input onChange={this.chooseTown} type="text" />
+            <div className="component">
+              Selected town: {this.state.selectedTown}
+            </div>
+          </div>
+          <div className="selector">
+            <Dropdown
+              className="dropdown"
+              options={prompts}
+              onChange={this.choosePrompt}
+              value="Select a Prompt"
+            />
+            <div className="component">
+              Selected prompt: {this.state.selectedPrompt}
+            </div>
+          </div>
+          {this.renderChosen()}
         </div>
-        {this.renderChosen()}
       </div>
     );
   }
